4ebe34334c8e92bf66d1a8c518a7d950ab4ae4e3
Trevor Bercher <trevorbercher@gmail.com>
2026-06-17 11:32:13 -0400

    Feat: Reimplemented spring skeleton key

---
+++ src/test/java/com/audio/audio_transcibe/AudioTranscibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audio.audio_transcibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioTranscibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
